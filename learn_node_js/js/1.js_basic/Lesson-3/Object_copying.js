c35e7e3daaef7c515970e8309fb3dbc45e735b6e
kadirov <tasaddux@gmail.com>
2021-03-27 23:51:28 +0500

    lessons added

---
+++ learn_node_js/js/1.js_basic/Lesson-3/Object_copying.js
@@ -0,0 +1,52 @@
+
+// console.log("user", user)
+
+
+
+// console.log("surnem",surname)
+// console.log("surname.name: ",surname.name)
+//surname.name = "Sher"
+// console.log("surname.name: ",surname.name)
+// console.log("user.name: ",user.name)
+// let myNumber = {};
+// let anotherNumber = myNumber;
+//console.log(anotherNumber == myNumber)
+
+//method 1
+// for (let key in user) {
+//     surname[key] = user[key]
+// }
+// console.log(surname)
+
+// surname.name = "Sher"
+
+// console.log("surname.name:", surname.name)
+// console.log("user.name:", user.name)
+
+//build in functions => Object.assign({}, obj_name)
+
+//method 1 with object.assign
+
+
+
+// let user = {
+//     name: "Ali",
+//     age: 24,
+// }
+
+// let surname = {};
+
+// surname = Object.assign(user, {name: "Sher"});
+
+//console.log(surname);
+
+// let person = {
+//     gender: "male"
+// }
+
+
+// let people;
+// people = Object.assign(surname, user, person)
+
+// console.log(people)
+
---
+++ learn_node_js/js/1.js_basic/Lesson-3/Objects.js
@@ -0,0 +1,52 @@
+//let user = new Object(); // object constructor
+
+//console.log(typeof user)
+
+//let user = {};
+//console.log(typeof user)
+
+// let car = {
+//     color: "White",
+//     name: "BMW",
+//     price: 10000,
+//     "manafucturer": "Germany",
+//     "made in year": 2000
+// }
+
+// console.log(car);
+// console.log('The name of car ', car.name);
+// console.log(car["price"])
+// console.log(car["made in year"])
+
+// function buyCar(name, price) {
+//     return {
+//         name: name,
+//         price: price,
+//     }
+// }
+
+// let car = buyCar("Audi", 20000);
+
+// console.log(car.name)
+
+// function buyCar2(name, price) {
+//     return {
+//         name,
+//         price,
+//     }
+// }
+// let car2 = buyCar2("Audi", 20000)
+
+// console.log(car2.name)
+
+// let user = {
+//     name: "Jack",
+//     age: 22,
+//     gender: "male"
+// }
+// //for .. in
+
+// for (let key in user) {
+//     console.log(user[key])
+//     console.log(key)
+// }
